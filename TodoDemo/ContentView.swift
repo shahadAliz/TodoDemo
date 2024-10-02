0dc127de44eb96eeb50f19611e360a697a5f6a3e
shahadAliz <cs.shahadz@gmail.com>
2024-10-02 06:34:03 +0300

    Initial Commit

---
+++ TodoDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoDemo
+//
+//  Created by شهد علي on 29/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoDemo/TodoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoDemoApp.swift
+//  TodoDemo
+//
+//  Created by شهد علي on 29/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct TodoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
